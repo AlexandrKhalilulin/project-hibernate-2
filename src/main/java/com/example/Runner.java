fc952b2ed549fdf1cf1b408f2ee404010479ec06
user <akhalilulin@mail.ru>
2022-12-08 13:23:02 +0300

    add ManagerDAO add ManagerDAO.createCustomer

--- src/main/java/com/example/Runner.java
+++ src/main/java/com/example/Runner.java
@@ -1,9 +1,10 @@
 package com.example;
 
 import com.example.factories.SessionFactory;
+import com.example.utils.ManagerDAO;
 
 public class Runner {
     public static void main(String[] args) {
-        SessionFactory.getSessionFactory();
+        ManagerDAO.getManagerDAO().createCustomer();
     }
 }
---
+++ src/main/java/com/example/utils/ManagerDAO.java
@@ -0,0 +1,74 @@
+package com.example.utils;
+
+import com.example.dao.*;
+import com.example.entities.*;
+import com.example.factories.SessionFactory;
+import org.hibernate.Session;
+
+public class ManagerDAO {
+    private static ManagerDAO instance;
+    private final org.hibernate.SessionFactory sessionFactory;
+    private final ActorDAO actorDAO;
+    private final AddressDAO addressDAO;
+    private final CategoryDAO categoryDAO;
+    private final CityDAO cityDAO;
+    private final CountryDAO countryDAO;
+    private final CustomerDAO customerDAO;
+    private final FilmDAO filmDAO;
+    private final FilmTextDAO filmTextDAO;
+    private final InventoryDAO inventoryDAO;
+    private final LanguageDAO languageDAO;
+    private final PaymentDAO paymentDAO;
+    private final RentalDAO rentalDAO;
+    private final StaffDAO staffDAO;
+    private final StoreDAO storeDAO;
+
+    public ManagerDAO() {
+        this.sessionFactory = SessionFactory.getSessionFactory();
+        actorDAO = new ActorDAO(sessionFactory);
+        addressDAO = new AddressDAO(sessionFactory);
+        categoryDAO = new CategoryDAO(sessionFactory);
+        cityDAO = new CityDAO(sessionFactory);
+        countryDAO = new CountryDAO(sessionFactory);
+        customerDAO = new CustomerDAO(sessionFactory);
+        filmDAO = new FilmDAO(sessionFactory);
+        filmTextDAO = new FilmTextDAO(sessionFactory);
+        inventoryDAO = new InventoryDAO(sessionFactory);
+        languageDAO = new LanguageDAO(sessionFactory);
+        paymentDAO = new PaymentDAO(sessionFactory);
+        rentalDAO = new RentalDAO(sessionFactory);
+        staffDAO = new StaffDAO(sessionFactory);
+        storeDAO = new StoreDAO(sessionFactory);
+    }
+
+    public static ManagerDAO getManagerDAO() {
+        if (instance == null) {
+            instance = new ManagerDAO();
+        }
+        return instance;
+    }
+
+    public Customer createCustomer(String firstName, String lastName, String currentAddress, String district, String phone, String currentCity){
+        try(Session session = sessionFactory.openSession()){
+            session.beginTransaction();
+            Store store = storeDAO.getById(0);
+            City city = cityDAO.getByName(currentCity);
+            Address address = new Address();
+            address.setAddress(currentAddress);
+            address.setDistrict(district);
+            address.setCity(city);
+            address.setPhone(phone);
+            addressDAO.save(address);
+            Customer customer = new Customer();
+            customer.setAddress(address);
+            customer.setActive(true);
+            customer.setStore(store);
+            customer.setFirstName(firstName);
+            customer.setLastName(lastName);
+            customerDAO.save(customer);
+            session.getTransaction().commit();
+            return customer;
+        }
+    }
+
+}
